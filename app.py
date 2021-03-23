54a67762710a802a458a47e6659a4230d03e9f9e
Abraham14711 <74873275+Abraham14711@users.noreply.github.com>
2021-03-23 13:15:00 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 def main_page():
     return render_template('pizza.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('Contacts.html')
+
+@app.route('/new')
+def new_things():
+    return render_template('New things.html')
+
+
+@app.route('/new1')
+def new1():
+    return render_template('new 1.html')
+
+@app.route('/new2')
+def new2():
+    return render_template('new 2.html')
 
 
 if __name__ == '__main__':
